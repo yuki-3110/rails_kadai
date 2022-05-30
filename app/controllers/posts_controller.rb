eed4841ac5098b044ca7e93130fe0ec641b6118d
yuki saito <ykst0622@gmail.com>
2022-05-30 22:24:31 +0900

    共通処理の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   def index
     @posts = Post.all
   end
@@ -25,10 +26,27 @@ class PostsController < ApplicationController
     @post=Post.find(params[:id])
   end
 
+  def edit
+    @post= Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
 
