21ce8202452cba65ade43db11c678e31a7467a40
yuki saito <ykst0622@gmail.com>
2022-05-30 14:32:51 +0900

    つぶやき一覧表示機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
     @post=Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to new_post_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   resources :posts
 end
