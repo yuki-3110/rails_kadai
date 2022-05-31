5829d05f1220df26d92693aff681e4aff173d03f
yuki saito <ykst0622@gmail.com>
2022-05-31 09:23:21 +0900

    フィードバックメッセージを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,15 +18,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post=Post.find(params[:id])
   end
 
   def edit
-    @post= Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: "ブログを編集しました！"
     else
